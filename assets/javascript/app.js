2215b97f84ebef8001d60e342395affd485ac0ff
Owner <Owner>
2017-02-25 16:54:08 -0600

    fully functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,14 +21,14 @@ var rightQuestions = 0, wrongQuestions = 0, unasweredQuestions = 0, time;
 //runs a timed interval, displays the amount of time remaining, and goes to results when the time runs out
 
 function twoMinutes(){
-        t=10;
+        t=120;
 
         timer = setInterval(function(){
         t--;
         console.log('t' + t);
         
         if(t>=0){
-            $('.timerSpan').text(t);
+            $('.timerBottom').text(t);
         }
         if(t==0){
             displayResults();
@@ -58,45 +58,181 @@ function twoMinutes(){
    	//triviaFunction end
    }
 
-
-
    function displayResults() {
    	console.log('displayResults is working');
   	$('.startDiv').attr('style', 'display: none');
    	$('.questionsDiv').attr('style', 'display: none');
    	$('.resultsDiv').attr('style', 'display: normal');
 
-          	//question 1
-   	if($('#1t').is(':checked')){
+   	//The section below compares user answers to actual answers. When I have more time I would like to set up a loop
+   	//to compare multiple arrays with both user answers and actual answers
+
+          	//question 1, a
+   	if($('#1a').is(':checked')){
    		rightQuestions += 1;
    	}
-   	else if($('#1f').is(':checked')){
+   	else if($('#1b').is(':checked')){
    		wrongQuestions += 1;
    	}
-   	else {
-   		unasweredQuestions += 1;
+   	else if($('#1c').is(':checked')){
+        wrongQuestions += 1;
    	}
-   	          	//question 2
-   	if($('#2f').is(':checked')){
-   		rightQuestions += 1;
-   	}
-   	else if($('#2t').is(':checked')){
-   		wrongQuestions += 1;
-   	}
-   	else {
-   		unasweredQuestions += 1;
-   	}
-   	   	         //question 3
-   	if($('#3f').is(':checked')){
-   		rightQuestions += 1;
-   	}
-   	else if($('#3t').is(':checked')){
-   		wrongQuestions += 1;
+  	else if($('#1d').is(':checked')){
+        wrongQuestions += 1;
    	}
    	else {
    		unasweredQuestions += 1;
    	}
 
+          	//question 2, d
+   	if($('#2a').is(':checked')){
+   		wrongQuestions += 1;
+   	}
+   	else if($('#2b').is(':checked')){
+   		wrongQuestions += 1;
+   	}
+   	else if($('#2c').is(':checked')){
+        wrongQuestions += 1;
+   	}
+  	else if($('#2d').is(':checked')){
+        rightQuestions += 1;
+   	}
+   	else {
+   		unasweredQuestions += 1;
+   	}
+
+          	//question 3, b
+   	if($('#3a').is(':checked')){
+   		wrongQuestions += 1;
+   	}
+   	else if($('#3b').is(':checked')){
+   		rightQuestions += 1;
+   	}
+   	else if($('#3c').is(':checked')){
+        wrongQuestions += 1;
+   	}
+  	else if($('#3d').is(':checked')){
+        wrongQuestions += 1;
+   	}
+   	else {
+   		unasweredQuestions += 1;
+   	}
+
+          	//question 4, a
+   	if($('#4a').is(':checked')){
+   		rightQuestions += 1;
+   	}
+   	else if($('#4b').is(':checked')){
+   		wrongQuestions += 1;
+   	}
+   	else if($('#4c').is(':checked')){
+        wrongQuestions += 1;
+   	}
+  	else if($('#4d').is(':checked')){
+        wrongQuestions += 1;
+   	}
+   	else {
+   		unasweredQuestions += 1;
+   	}
+
+   	          	//question 5, c
+   	if($('#5a').is(':checked')){
+   		wrongQuestions += 1;
+   	}
+   	else if($('#5b').is(':checked')){
+   		wrongQuestions += 1;
+   	}
+   	else if($('#5c').is(':checked')){
+        rightQuestions += 1;
+   	}
+  	else if($('#5d').is(':checked')){
+        wrongQuestions += 1;
+   	}
+   	else {
+   		unasweredQuestions += 1;
+   	}
+
+   	          	//question 6, b
+   	if($('#6a').is(':checked')){
+   		wrongQuestions += 1;
+   	}
+   	else if($('#6b').is(':checked')){
+   		rightQuestions += 1;
+   	}
+   	else if($('#6c').is(':checked')){
+        wrongQuestions += 1;
+   	}
+  	else if($('#6d').is(':checked')){
+        wrongQuestions += 1;
+   	}
+   	else {
+   		unasweredQuestions += 1;
+   	}
+
+   	          	//question 7, d
+   	if($('#7a').is(':checked')){
+   		wrongQuestions += 1;
+   	}
+   	else if($('#7b').is(':checked')){
+   		wrongQuestions += 1;
+   	}
+   	else if($('#7c').is(':checked')){
+        wrongQuestions += 1;
+   	}
+  	else if($('#7d').is(':checked')){
+        rightQuestions += 1;
+   	}
+   	else {
+   		unasweredQuestions += 1;
+   	}
+          	//question 8, a
+   	if($('#8a').is(':checked')){
+   		rightQuestions += 1;
+   	}
+   	else if($('#8b').is(':checked')){
+   		wrongQuestions += 1;
+   	}
+   	else if($('#8c').is(':checked')){
+        wrongQuestions += 1;
+   	}
+  	else if($('#8d').is(':checked')){
+        wrongQuestions += 1;
+   	}
+   	else {
+   		unasweredQuestions += 1;
+   	}
+          	//question 9, c
+   	if($('#9a').is(':checked')){
+   		wrongQuestions += 1;
+   	}
+   	else if($('#9b').is(':checked')){
+   		wrongQuestions += 1;
+   	}
+   	else if($('#9c').is(':checked')){
+        rightQuestions += 1;
+   	}
+  	else if($('#9d').is(':checked')){
+        wrongQuestions += 1;
+   	}
+   	else {
+   		unasweredQuestions += 1;
+   	}
+          	//question 10, b
+   	if($('#10a').is(':checked')){
+   		wrongQuestions += 1;
+   	}
+   	else if($('#10b').is(':checked')){
+   		rightQuestions += 1;
+   	}
+   	else if($('#10c').is(':checked')){
+        wrongQuestions += 1;
+   	}
+  	else if($('#10d').is(':checked')){
+        wrongQuestions += 1;
+   	}
+   	else {
+   		unasweredQuestions += 1;
+   	}
 
 
    	//
